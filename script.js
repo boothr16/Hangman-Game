88d69d339f056fceddb6980723f3bf9ee76eac0c
Robert Booth <boothr16@gmail.com>
2021-08-17 13:12:25 -0400

    Cached main elements from DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+const wordElement = document.getElementById('word');
+const wrontLettersElement = document.getAnimations('wrong-letters');
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMsg = document.getElementById('final-message');
