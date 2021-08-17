7999b388bb09855c3d8b254e3f72ae6b84dc5a3d
Robert Booth <boothr16@gmail.com>
2021-08-17 15:17:43 -0400

    displayWord() completed

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const words = ['application', 'programming', 'interface', 'wizard']; // random w
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['w', 'i', 'z', 'a', 'r', 'd'];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
