8fa125372cfe01a4e743281e0a0ae9b28dd11c30
Robert Booth <boothr16@gmail.com>
2021-09-10 22:00:42 -0400

    Add figure parts on wrong guess

--- script.js
+++ script.js
@@ -42,6 +42,17 @@ function updateWrongLetters() {
     ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
     ${wrongLetters.map(letter => `<span>${letter}</span`)}
     `;
+
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if (index < errors) {
+            part.style.display = 'block';
+        }
+        else {
+            part.style.display = 'none';
+        }
+    });
 }
 
 // Show notification
